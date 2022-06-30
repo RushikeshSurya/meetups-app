c6d8a95036e1e496bc6c720664054a1a12ed64ef
rushikesh.kumar <rushikesh.kumar@kaptyn.com>
2022-06-30 16:21:45 +0530

    ready for deployment

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,54 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+import {MongoClient,ObjectId} from 'mongodb';
+
+function MeetupDetails(props) {
+  return (
+    <MeetupDetail
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
+    />
+  );
+}
+
+export async function getStaticPaths(){
+  const client = await MongoClient.connect('mongodb+srv://rushikesh9811:ScareCrow9811@cluster0.yqj7ceg.mongodb.net/meetups?retryWrites=true&w=majority')
+    const db =client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const meetups=await meetupsCollection.find({}, {_id: 1}).toArray();
+    client.close();
+  return{
+    fallback:false,
+    paths:
+    meetups.map(meetup=>({
+      params:{meetupId:meetup._id.toString() },
+  })),
+  };
+} 
+
+export async function  getStaticProps(context){
+
+  const meetupId =context.params.meetupId;
+  
+  const client = await MongoClient.connect('mongodb+srv://rushikesh9811:ScareCrow9811@cluster0.yqj7ceg.mongodb.net/meetups?retryWrites=true&w=majority')
+  const db =client.db();
+
+  const meetupsCollection = db.collection('meetups');
+  const selectedMeetup=await meetupsCollection.findOne({_id: ObjectId(meetupId)});
+  client.close();
+  return {
+      props:{
+        meetupData:{
+          id:selectedMeetup._id.toString(),
+          image:selectedMeetup.image,
+          title:selectedMeetup.title,
+          description:selectedMeetup.description,
+          address:selectedMeetup.address,
+        }
+      }
+  }
+}
+
+export default MeetupDetails;
