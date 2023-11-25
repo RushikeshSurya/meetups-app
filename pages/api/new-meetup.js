21977ffe5fa9d33121601831d1071dec6108ddb9
Rushikesh Rajkumar Suryawanshi <68456042+RushikeshSurya@users.noreply.github.com>
2023-11-25 20:42:16 +0530

    updating mongodb connection string

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -3,7 +3,7 @@ import {MongoClient} from 'mongodb';
 async function handler(req, res){
 if(req.method==="POST"){
     const data = req.body;
-    const client = await MongoClient.connect('mongodb+srv://rushikesh9811:ScareCrow9811@cluster0.yqj7ceg.mongodb.net/meetups?retryWrites=true&w=majority')
+    const client = await MongoClient.connect('mongodb+srv://rushikeshsuryawanshi9811:vpg5ih2p546bq2Ko@cluster0.bzsnqqx.mongodb.net/meetups')
     const db =client.db();
 
     const meetupsCollection = db.collection('meetups');
